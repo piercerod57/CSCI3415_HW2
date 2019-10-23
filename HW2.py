3133c114c32af9f6fea6dd7aad7248524c6b828e
Pierce Hopkins <piercerod57@gmail.com>
2019-10-23 15:52:01 -0600

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -8,28 +8,24 @@ import math
 import itertools  
 import collections 
 
-def main():
-	#--------------Class-Definitions--------------
-	class Year:
-		def __init__(self, year, maleDict, femaleDict):
-			self.year = year
-			self.maleDict = maleDict
-			self.femaleDict = femaleDict
+#--------------Class-Definitions--------------
+class Year:
+	def __init__(self, year, yearMaleDict, yearFemaleDict):
+		self.year = year
+		self.maleDict = yearMaleDict
+		self.femaleDict = yearFemaleDict
 	
-	class Decade:
-		def __init__(self, year):
-			self.year = year
-			self.maleDict = dict()
-			self.femaleDict = dict()
-	#--------------Class-Definitions--------------
-	
-	#Open the files and read the contents
-	#-----------------File-Read-----------------
-	years = os.listdir('./data/')
-	
+class Decade:
+	def __init__(self, year):
+		self.year = year
+		self.genderDict = dict()
+#--------------Class-Definitions--------------
+
+
+#-----------------File-Read-----------------
+def ReadFiles():
 	yearArray = []
-	decades = []
-	
+	years = os.listdir('./data/')
 	for year in years:
 		
 		yearMaleDict = {}
@@ -50,12 +46,17 @@ def main():
 		
 		newYear = Year(year.strip("yob.txt"), yearMaleDict, yearFemaleDict)
 		yearArray.append(newYear)
-	#-----------------File-Read-----------------
-	
-	
-	#---------------Parse-Decades---------------
+		
+		
+	return yearArray
+#-----------------File-Read-----------------
+
+
+#---------------Parse-Decades---------------
+def ParseDecades(yearArray, gender):
+	decades = []
 	decadesLength = math.ceil(len(yearArray)/10)
-	print("Data found for %s decades:"%(decadesLength))
+	print(gender, "data found for %s decades:"%(decadesLength))
 	
 	for i in range(decadesLength):
 		print("\t The %s's"%(yearArray[i*10].year))
@@ -63,47 +64,75 @@ def main():
 		decades.append(newDecade)
 		for j in range(10):
 			if (( i * 10 ) + j) < len(yearArray):
-				#print(yearArray[(( i * 10 ) + j)]["year"])
-				#@NOTE(P): Break these for loops into function?
-				newMaleDict = collections.defaultdict(int)
-				for key, val in itertools.chain(decades[i].maleDict.items(), yearArray[(( i * 10 ) + j)].maleDict.items()):
-					newMaleDict[key] += val
-				
-				decades[i].maleDict.clear()
-				decades[i].maleDict = newMaleDict.copy()
-				
-				newFemaleDict = collections.defaultdict(int)
-				for key, val in itertools.chain(decades[i].femaleDict.items(), yearArray[(( i * 10 ) + j)].femaleDict.items()): 
-					newFemaleDict[key] += val
+				if gender == "Male":
+					newMaleDict = collections.defaultdict(int)
+					for key, val in itertools.chain(decades[i].genderDict.items(), yearArray[(( i * 10 ) + j)].maleDict.items()):
+						newMaleDict[key] += val
 					
-				decades[i].femaleDict.clear()
-				decades[i].femaleDict = newFemaleDict.copy()
+					decades[i].genderDict.clear()
+					decades[i].genderDict = newMaleDict.copy()
+				elif gender == "Female":
+					newFemaleDict = collections.defaultdict(int)
+					for key, val in itertools.chain(decades[i].genderDict.items(), yearArray[(( i * 10 ) + j)].femaleDict.items()): 
+						newFemaleDict[key] += val
+						
+					decades[i].genderDict.clear()
+					decades[i].genderDict = newFemaleDict.copy()
 			else:
 				break
-	#---------------Parse-Decades---------------
-	
-	#--------------Print-By-Decade--------------
-	cnt = 1
-	for decade in decades:#@NOTE(P): Currently not sorted by value, just returns 10 names regardless of value
-		print("The top 10 most popular names in the %s's"%(decade.year))
-		print("Males:")
-		for key,val in decade.maleDict.items():
-			if cnt == 11:
-				cnt = 1
-				break
-			else:
-				print("\t", cnt, " ", key, "\t", val)
-				cnt+=1
-		print("Females:")
-		for key,val in decade.femaleDict.items():
-			if cnt == 11:
-				cnt = 1
-				break
-			else:
-				print("\t", cnt, " ", key, "\t", val)
-				cnt+=1
-			
-	#--------------Print-By-Decade--------------
+				
+	return decades
+#---------------Parse-Decades---------------
 
+
+#--------------Print-By-Decade--------------
+def PrintTopTenNamesByDecade(decades, gender):#@NOTE(P): return array with top10 names
+	top10GenderNamesByDecade = []
+	
+	cnt = 1
+	for decade in decades:
+		newDecade = Decade(decade.year)
+		print("The top 10 most popular ", gender, " names in the %s's"%(decade.year))
+		for key,val in sorted(decade.genderDict.items(), key=lambda x: x[1], reverse=True):
+			if cnt == 11:
+				cnt = 1
+				break
+			else:
+				print("\t", cnt, " ", key, "\t", val)
+				newDecade.genderDict[key] = val
+				cnt+=1
+		top10GenderNamesByDecade.append(newDecade)
+	print('\n')
+	
+	return top10GenderNamesByDecade
+#--------------Print-By-Decade--------------
+
+
+#----------------Name-Rankings---------------
+def PrintNameRankings(decades, gender):
+	print(gender, " Name Rankings (by Name):")
+	print('\t', '\t', end =" ")
+	for decade in decades:
+		print(decade.year, '\t', end =" ")
+	print('\n')
+	
+	
+#----------------Name-Rankings---------------
+
+
+#-------------------Main--------------------
+def main():
+	#@NOTE(P): Open the files and read the contents
+	yearArray = ReadFiles();
+	#NOTE(P): Sort the entries into arrays of Python Dictionaries
+	decadesMale = ParseDecades(yearArray, "Male")
+	decadesFemale = ParseDecades(yearArray, "Female")
+	
+	top10MaleNamesByDecade = PrintTopTenNamesByDecade(decadesMale, "Male")
+	top10FemaleNamesByDecade = PrintTopTenNamesByDecade(decadesFemale, "Female")
+	
+	PrintNameRankings(top10MaleNamesByDecade, "Male")
+	PrintNameRankings(top10FemaleNamesByDecade, "Female")
+#-------------------Main--------------------
 if __name__ == '__main__':
 	main()
